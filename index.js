8926aa6cfda533f20197b98f1f84f85f5f781b3b
HieuNT1998 <40411081+HieuNT1998@users.noreply.github.com>
2018-09-14 11:23:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
 
+app.listen( process.env.PORT || 1998, (err) => {
+    if (err) console.log(err);
+    else console.log("Server ready !");
+}) 
 
-app.listen(process.env.PORT || 1998,(err)=>{
-    if(err) console.log(err);
-    else console.log("thanh cong");
-});
 
